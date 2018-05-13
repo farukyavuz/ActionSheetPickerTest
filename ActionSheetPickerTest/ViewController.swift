0449312c7e9c24779060b33a4bd7e5ba8e2a1795
Faruk Yavuz <faruk@email.com>
2018-05-13 23:39:34 +0300

    initial commit

--- ActionSheetPickerTest/ViewController.swift
+++ ActionSheetPickerTest/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import ActionSheetPicker_3_0
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +23,42 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func buttonTabbed(_ sender: Any) {
+//        ActionSheetStringPicker.show(withTitle: "Eviniz Kaç m²?", rows: ["0-150 m²", "150-300 m²", "300-450 m²"], initialSelection: 1, doneBlock: {
+//            picker, value, index in
+//
+//            print("value = \(value)")
+//            print("index = \(index)")
+//            print("picker = \(picker)")
+//            return
+//        }, cancel: { ActionStringCancelBlock in return }, origin: sender)
+        
+        
+        let cancelButton = getBarButton("İptal")
+        let doneButton = getBarButton("Tamamla")
+
+        let actionSheetStringPicker = ActionSheetStringPicker(title: "Eviniz Kaç m²?", rows: ["0-150 m²", "150-300 m²", "300-450 m²"], initialSelection: 0, doneBlock: { (picker : ActionSheetStringPicker?, selectedIndex : Int, selectedItem : Any?) in
+
+        }, cancel: { (picker : ActionSheetStringPicker?) in
+
+        }, origin: sender)
+
+        actionSheetStringPicker!.setCancelButton(cancelButton)
+        actionSheetStringPicker!.setDoneButton(doneButton)
+        actionSheetStringPicker!.show()
+        
+    }
+    
+    func getBarButton(_ title : String) -> UIBarButtonItem{
+        let customButton =  UIButton.init(type: UIButtonType.custom)
+        customButton.setTitle(title, for: .normal)
+        customButton.setTitleColor(self.view.tintColor, for: UIControlState.normal)
+        //customButton.roundCorner(5)
+        customButton.frame = CGRect.init(x: 0, y: 5, width: 80, height: 32)
+        //customButton.backgroundColor = UIColor.green
+        return UIBarButtonItem.init(customView: customButton)
+    }
+    
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+xcodeproj 'ActionSheetPickerTest.xcodeproj'
+platform :ios, '10.3'
+
+target 'ActionSheetPickerTest' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ActionSheetPickerTest
+	pod 'ActionSheetPicker-3.0'
+end
